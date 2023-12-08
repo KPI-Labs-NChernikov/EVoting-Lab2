e8c0120bc6e7abf1582d2fd37d166294f97a39f3
Chernikov Nikita <14nik20@gmail.com>
2023-12-08 09:11:32 +0200

    Implementing CentralElectionCommission

--- Modelling/Entities/CentralElectionCommission.cs
+++ Modelling/Entities/CentralElectionCommission.cs
@@ -1,4 +1,5 @@
 ﻿using Algorithms.Abstractions;
+using Algorithms.Common;
 using FluentResults;
 using Modelling.Entities;
 using Modelling.Models;
@@ -21,9 +22,12 @@ public sealed class CentralElectionCommission
 
     public CentralElectionCommission(IEnumerable<Candidate> candidates, IEnumerable<Voter> voters, IMaskedAsymmetricAlgorithmKeysGenerator<RSAParameters> keysGenerator)
     {
+        VotingResults = new();
+
         foreach (var candidate in candidates)
         {
             _candidates.Add(candidate.Id, candidate);
+            VotingResults.CandidatesResults.Add(candidate.Id, new (candidate));
         }
 
         foreach (var voter in voters)
@@ -33,17 +37,117 @@ public sealed class CentralElectionCommission
         }
 
         (PublicKey, _privateKey) = keysGenerator.GenerateKeys();
-        VotingResults = new();
     }
 
-    public Result<IEnumerable<byte[]>> AcceptBatches()
+    public Result<IEnumerable<byte[]>> AcceptBatches(byte[] collection, IRSAService rsaService, IObjectToByteArrayTransformer objectToByteTransformer, IRandomProvider randomProvider)
     {
-
+        return CheckIfVotingIsCompleted()
+            .Bind(() => DecryptBallotBatchesCollection(collection, rsaService, objectToByteTransformer))
+            .Bind(c => CheckBatches())
     }
 
-    public Result<int> AcceptVote()
+    private Result<BallotBatchesCollection> DecryptBallotBatchesCollection(byte[] collection, IRSAService rsaService, IObjectToByteArrayTransformer objectToByteArrayTransformer)
     {
+        return Result.Try(()
+            => objectToByteArrayTransformer.ReverseTransform<BallotBatchesCollection>(rsaService.Decrypt(collection, _privateKey))
+                ?? throw new InvalidOperationException("Value cannot be transformed to ballot batches collection ballot."),
+            e => new Error("Message has wrong format or was incorrectly encrypted.").CausedBy(e));
+    }
 
+    private Result<IEnumerable<byte[]>> CheckBatches(BallotBatchesCollection collection, IRSAService rsaService, IObjectToByteArrayTransformer objectToByteArrayTransformer, IRandomProvider randomProvider)
+    {
+        var skipBatch = randomProvider.NextItem(collection.Batches);
+        foreach (var batch in collection.Batches) 
+        {
+            if (batch == skipBatch)
+            {
+                continue;
+            }
+
+            var batchCheckResult = Result.Ok()
+                .Bind();
+        }
+    }
+
+    private Result<BallotBatch> CheckBatch(BallotBatch batch, byte[] maskMultiplier, IRSAService rsaService, IObjectToByteArrayTransformer objectToByteArrayTransformer)
+    {
+        foreach (var maskedBallot in batch.MaskedBallots)
+        {
+
+        }
+    }
+
+    public Result<int> AcceptVote(byte[] encryptedSignedBallot, IRSAService rsaService, IObjectToByteArrayTransformer objectToByteTransformer)
+    {
+        return CheckIfVotingIsCompleted()
+            .Bind(() => DecryptSignedBallot(encryptedSignedBallot, rsaService, objectToByteTransformer))
+            .Bind(sb => VerifySignature(sb, rsaService, objectToByteTransformer))
+            .Bind(VerifyVoterWhileVoting)
+            .Bind(VerifyCandidate)
+            .Bind(AddVote);
+    }
+
+    private Result CheckIfVotingIsCompleted()
+    {
+        return Result.FailIf(IsVotingCompleted, new Error("The voting is already completed."));
+    }
+
+
+    private Result<SignedBallot> DecryptSignedBallot(byte[] encryptedSignedBallot, IRSAService rsaService, IObjectToByteArrayTransformer objectToByteArrayTransformer)
+    {
+        return Result.Try(()
+            => objectToByteArrayTransformer.ReverseTransform<SignedBallot>(rsaService.Decrypt(encryptedSignedBallot, _privateKey))
+                ?? throw new InvalidOperationException("Value cannot be transformed to signed ballot."),
+            e => new Error("Message has wrong format or was incorrectly encrypted.").CausedBy(e));
+    }
+
+    private Result<Ballot> VerifySignature(SignedBallot signedBallot, IRSAService rsaService, IObjectToByteArrayTransformer objectToByteArrayTransformer)
+    {
+        var signatureIsAuthentic = rsaService.VerifyHash(objectToByteArrayTransformer.Transform(signedBallot.Ballot), signedBallot.Signature, PublicKey);
+
+        if (!signatureIsAuthentic)
+        {
+            return Result.Fail(new Error("The signature is not authentic."));
+        }
+
+        return Result.Ok(signedBallot.Ballot);
+    }
+
+    private Result<Ballot> VerifyVoterWhileVoting(Ballot ballot)
+    {
+        var voterWasFound = _voters.TryGetValue(ballot.VoterId, out var voter);
+        if (!voterWasFound)
+        {
+            return Result.Fail(new Error("The voter was not found."));
+        }
+
+        var voterHasVoted = _votersStatuses[ballot.VoterId] == VoterStatus.Voted;
+        if (voterHasVoted)
+        {
+            return Result.Fail(new Error("The voted has already casted a vote."));
+        }
+
+        return Result.Ok(ballot);
+    }
+
+    private Result<Ballot> VerifyCandidate(Ballot ballot)
+    {
+        var candidateWasFound = _candidates.ContainsKey(ballot.CandidateId);
+        if (!candidateWasFound)
+        {
+            return Result.Fail(new Error("Candidate was not found."));
+        }
+
+        return Result.Ok(ballot);
+    }
+
+    private Result<int> AddVote(Ballot ballot)
+    {
+        VotingResults.CandidatesResults[ballot.CandidateId].Votes++;
+        var ballotNumber = VotingResults.VotersResults.Count + 1;
+        VotingResults.VotersResults.Add(new(ballot.VoterId, ballotNumber, ballot.CandidateId));
+
+        return Result.Ok(ballotNumber);
     }
 
     public void CompleteVoting()
--- Modelling/Entities/VotingResults.cs
+++ Modelling/Entities/VotingResults.cs
@@ -3,7 +3,7 @@
 namespace Modelling.Models;
 public sealed class VotingResults
 {
-    public ICollection<CandidateVotingResults> CandidatesResults { get; } = new List<CandidateVotingResults>();
+    public SortedDictionary<int, CandidateVotingResults> CandidatesResults { get; } = new ();
 
-    public ICollection<VoterResults> VoterResults { get; } = new List<VoterResults>();
+    public ICollection<VoterResults> VotersResults { get; } = new List<VoterResults>();
 }
